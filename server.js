eed1fa0d70c83b53c0658b304cc74f7a28aa5696
Ssophia <susu94827@gmail.com>
2023-03-02 16:49:32 -0500

    created a middleware folder and set up 404 error feedback

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('server')
+import express from 'express';
+import notFoundMiddleware from './middleware/not-found.js';
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('welcome')
+})
+
+app.use(notFoundMiddleware)
+
+const port = process.env.PORT || 5005;
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}...`)
+})
