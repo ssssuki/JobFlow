fecf126162956daf7eb95a408fb4a70b5a955bd4
susu xiang <susuxiang@susudeMac-mini.local>
2023-03-09 20:57:48 -0500

    set up login feature

--- client/src/context/reducers.js
+++ client/src/context/reducers.js
@@ -38,6 +38,25 @@ const reducer = (state, action) => {
       showAlert: true, alertType: 'danger', alertText: action.payload.msg,
     }
   }
+  if (action.type === LOGIN_USER_BEGIN) {
+    return { ...state, isLoading: true }
+  }
+
+  if (action.type === LOGIN_USER_SUCCESS) {
+    return {
+      ...state, isLoading: false, token: action.payload.token,
+      user: action.payload.user, userLocation: action.payload.Location,
+      jobLocation: action.payload.location,
+      showAlert: true, alertType: 'success',
+      alertText: 'Login Successful! Redirecting...',
+    }
+  }
+  if (action.type === LOGIN_USER_ERROR) {
+    return {
+    ...state, isLoading: false,
+      showAlert: true, alertType: 'danger', alertText: action.payload.msg,
+    }
+  }
 
   throw new Error(`no such action : ${action.type}`);
     };
